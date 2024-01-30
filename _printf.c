369e7cd44d3645d48b5a9d713cc87792cc4afa6d
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-01-31 01:13:14 +0300

    Fix: incorrect use of write function

--- _printf.c
+++ _printf.c
@@ -6,18 +6,18 @@
  */
 int _printf(const char *format, ...)
 {
-	int j, k = 0;
+	int j = 0;
 	int count = 0;
 	char *string;
 	va_list list;
 
 	va_start(list, format);
-	for (j = 0; format[j] != '\0' && format != NULL; j++)
+	for (j = 0; format != NULL && format[j] != '\0'; j++)
 	{
 		if (format[j] == '%' && format[j + 1] == 'c')
 		{
 			string = va_arg(list, char *);
-			write(1, &string, 1);
+			write(1, string, 1);
 			j++;
 			count++;
 		}
@@ -26,7 +26,7 @@ int _printf(const char *format, ...)
 			string = va_arg(list, char *);
 			while (*string != '\0')
 			{
-				write(1, string, k);
+				write(1, string, 1);
 				count++;
 				string++;
 			}
