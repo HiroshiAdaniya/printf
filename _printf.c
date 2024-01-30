46dd24ebfcd8d31f6445982773cf5bbc49efbd53
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-01-30 17:06:15 +0200

    updating _prnitf.c

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@ int _printf(const char *format, ...)
 	int ch = 0;
 	char *string;
 	va_list list;
-	
+	/* note for one of the requirements: %n stores the number of characters printed */
 	while (format[i] != '\0')
 		i++;
 
@@ -23,16 +23,15 @@ int _printf(const char *format, ...)
 			string = va_arg(list, char *);
 			write(1, &string, 1);
 			j++;
-			ch++;
 		}
 		else if (format[j] == '%' && format[j + 1] == 's')
 		{
+			k = 0;
 			string = va_arg(list, char *);
 			while (string != NULL && string[k] != '\0')
 				k++;
 			write(1, string, k);
 			j++;
-			ch++;
 		}
 		else if (format[j] == '%' && format[j + 1] == '%')
 		{
@@ -40,12 +39,9 @@ int _printf(const char *format, ...)
 			j++;
 		}
 		else
-		{
 			write(1, &format[j], 1);
-			ch++;
-		}
-		k = 0;
 	}
 	va_end(list);
-	exit(ch);
+	/* should return with a variable that stores data form %n*/
+	return (0);
 }
