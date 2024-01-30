082727276fa27240c44246a6561a1cb5ff4b8dfa
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-01-31 00:21:51 +0300

    Update: Hiro's recommended changes

--- _printf.c
+++ _printf.c
@@ -7,41 +7,47 @@
 int _printf(const char *format, ...)
 {
 	int i, j, k = 0;
-	int ch = 0;
+	int count = 0;
 	char *string;
 	va_list list;
-	/* note for one of the requirements: %n stores the number of characters printed */
-	while (format[i] != '\0')
-		i++;
+	/* note for one of the requirements: %n st*/
 
 	va_start(list, format);
 	
-	for (j = 0; j < i; j++)
+	for (j = 0; format[j] != '\0' && format != NULL; j++)
 	{
 		if (format[j] == '%' && format[j + 1] == 'c')
 		{
 			string = va_arg(list, char *);
 			write(1, &string, 1);
 			j++;
+			count++;
 		}
 		else if (format[j] == '%' && format[j + 1] == 's')
 		{
 			k = 0;
 			string = va_arg(list, char *);
 			while (string != NULL && string[k] != '\0')
+			{
+				write(1, string, k);
 				k++;
-			write(1, string, k);
+				count++;
+			}
 			j++;
 		}
 		else if (format[j] == '%' && format[j + 1] == '%')
 		{
 			write(1, "%", 1);
 			j++;
+			count++;
 		}
 		else
+		{
 			write(1, &format[j], 1);
+			count++;
+		}
 	}
 	va_end(list);
 	/* should return with a variable that stores data form %n*/
-	return (0);
+	return (count);
 }
