19b9cea23dd12559769ebabbc9de6222abbda17a
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-01-30 21:29:25 +0300

    Fix: some issues

--- _printf.c
+++ _printf.c
@@ -1,23 +1,21 @@
 #include "main.h"
-#include <stdio.h>  // Include necessary headers
+#include <stdio.h>
 #include <stdarg.h>
 
 /**
  * _printf - prints formatted output to stdout
  * @format: character string containing format specifiers
- * Return: number of characters printed
+ * Return: number of characters printed (excluding the null byte)
  */
 int _printf(const char *format, ...)
 {
-    int i = 0, j, k = 0, ch = 0;  // Initialize variables
+    int j, k = 0, ch = 0;
     char *string;
     va_list list;
 
-    va_start(list, format);  
-    while (format[i] != '\0')
-        i++;
+    va_start(list, format);
 
-    for (j = 0; j < i; j++)
+    for (j = 0; format[j] != '\0'; j++)
     {
         if (format[j] == '%' && format[j + 1] == 'c')
         {
@@ -26,13 +24,13 @@ int _printf(const char *format, ...)
             ch++;
             j++;
         }
-        else if (format[j] == '%' && format[j + 1] == 's')
+        else if (format[j] == '%' && format[j + 1] == 's')  // Handle %s specifier
         {
             k = 0;
             string = va_arg(list, char *);
-            while (string != NULL && string[k] != '\0')  // Calculate length of string
+            while (string != NULL && string[k] != '\0')
                 k++;
-            printf("%s", string);  // Print the string using printf
+            write(1, string, k);
             ch += k;
             j++;
         }
