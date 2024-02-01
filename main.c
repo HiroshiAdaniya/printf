c2431e6b89b2297a57fa813a60b7f8e2c5220a08
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-02-01 09:55:56 +0200

    added: checks to see if va_arg returns null

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+int main()
+{
+	_printf("hello ");
+	_printf("Hello world");
+	_printf("\n");
+	_printf("%%");
+	_printf("Hello\n");
+	_printf("This is a string test%s");
+	return (0);
+}
---
+++ mains.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+	printf("%c, hello\n");
+	return (0);
+}
